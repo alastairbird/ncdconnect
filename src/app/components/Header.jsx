b06eea344f41386dda601fcce3dcb7b88bc6b6b0
Alastair <design@alastairbird.co.uk>
2018-02-07 19:06:54 +0000

    temp

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -4,18 +4,32 @@ import { Link } from 'react-router-dom'
 const styles = require('../../sass/components/header.scss')
 const logoSrc = require('../../assets/ncdc-logo.svg')
 
-export class Header extends React.Component {
+interface HeaderProps extends React.Component {
+
+}
+
+interface HeaderState extends React.Component {
+  navigationOpen: boolean
+}
+
+export class Header extends React.Component<HeaderProps, HeaderState> {
 
   constructor(props) {
     super(props);
+    this.state = {navigationOpen: false,}
+     this.togglenNavigation = this.togglenNavigation.bind(this);
   }
 
+  // = () => not supported....
+  togglenNavigation() {
+    this.setState({navigationOpen: !this.state.navigationOpen})
+  }
 
   render() {
     return (
       <header className={styles.header}>
         <div className={styles.menuSearchWrapper}>
-          <a className={styles.menuButt}><i className="icon-menu__base"></i></a>
+          <a onClick={this.togglenNavigation} className={styles.menuButt}><i className="icon-menu__base"></i></a>
           <a className={styles.menuButt}><i className="icon-magnifying-glass__base"></i></a>
         </div>
 
@@ -24,6 +38,12 @@ export class Header extends React.Component {
         <div className={styles.menuProfileWrapper}>
           <a className={styles.menuButt}><i className="icon-user__base"></i></a>
         </div>
+        {this.state.navigationOpen && 
+          <nav>
+            <h1>YO</h1>
+          </nav>
+        }
+        
       </header>
     );
   }
