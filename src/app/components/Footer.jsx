2b9d6f3af10fa5aa3be4f4502e1e28727c464f93
Alastair <design@alastairbird.co.uk>
2018-02-12 21:07:41 +0000

    Adds a lean footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 
 const styles = require('../../sass/components/footer.scss')
+const logoSrc = require('../../assets/ncdc-logo-plain.svg');
 
 export class Footer extends React.Component {
 
@@ -12,9 +13,14 @@ export class Footer extends React.Component {
   render() {
     return (
       <footer className={styles.footer}>
-        <small>
-         NCDC \\ 2018
-        </small>
+        <img className={styles.logo} src={logoSrc} width="" height="" alt="NCD Connect logo" />
+        <ul className={styles.nav}>
+          <li><Link to='/'>Connect</Link></li>
+          <li><Link to='/'>Opportunities</Link></li>
+          <li><Link to='/'>Events</Link></li>
+          <li><Link to='/'>About</Link></li>
+          <li><Link to='/'>Contact</Link></li>
+        </ul>
       </footer>
     );
   }
