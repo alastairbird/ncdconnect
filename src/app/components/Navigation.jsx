3c7a391478af32ee1b1b4c4a3adeb3a8a3a0fa7e
Alastair <design@alastairbird.co.uk>
2018-02-27 20:59:09 +0000

    Sorts out navigation

--- src/app/components/Navigation.jsx
+++ src/app/components/Navigation.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import classNames from 'classnames';
 import { Link } from 'react-router-dom';
-const calSrc = require('../../assets/calendar.svg')
-const chatSrc = require('../../assets/chat.svg')
-const newsSrc = require('../../assets/newspaper.svg')
+const calSrc = require('../../assets/calendar-white.svg')
+const chatSrc = require('../../assets/chat-white.svg')
+const newsSrc = require('../../assets/newspaper-white.svg')
+const mapSrc = require('../../assets/planet-earth.svg')
 
 const styles = require('../../sass/components/navigation.scss')
 
@@ -28,18 +29,20 @@ export class Navigation extends React.Component<NavigationProps, NavigationState
           <Link className={styles.link} onClick={this.toggleNavigation} to='/Login'><i className="icon-user__base"></i>Register / Login</Link>
         </div>
         <div className={styles.navWrapper}>
-          <Link className={classNames(styles.link, styles.linkConnect)} onClick={this.toggleNavigation} to='/Connect'>
+          <Link className={styles.link} onClick={this.toggleNavigation} to='/Connect'>
             <img className={styles.ctaIcon} src={chatSrc} alt="" />Connect
-            </Link>
-          <Link className={classNames(styles.link, styles.linkNews)} onClick={this.toggleNavigation} to='/Opportunities'>
+          </Link>
+          <Link className={styles.link} onClick={this.toggleNavigation} to='/Opportunities'>
             <img className={styles.ctaIcon} src={newsSrc} alt="" />Opportunities
-            </Link>
-          <Link className={classNames(styles.link, styles.linkCal)} onClick={this.toggleNavigation} to='/Events'>
+          </Link>
+          <Link className={styles.link} onClick={this.toggleNavigation} to='/Events'>
             <img className={styles.ctaIcon} src={calSrc} alt="" />Events
-            </Link>
+          </Link>
+          <Link className={styles.link} onClick={this.toggleNavigation} to='/WorldMap'>
+            <img className={styles.ctaIcon} src={mapSrc} alt="" />Map
+          </Link>
           <div className={styles.botlinks}>
             <Link className={styles.link} onClick={this.toggleNavigation} to='/'>Home</Link>
-            <Link className={styles.link} onClick={this.toggleNavigation} to='/WorldMap'>Map</Link>
             <Link className={styles.link} onClick={this.toggleNavigation} to='/About'>About</Link>
             <Link className={styles.link} onClick={this.toggleNavigation} to='/Contact'>Contact</Link>
           </div>
